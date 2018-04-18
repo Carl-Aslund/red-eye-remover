197d3cfce4bdd13ccbc810b44fef50ffd09db052
Carl Aslund <caslund@g.hmc.edu>
2018-04-18 15:41:43 -0700

    Minor changes

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import numpy as np
 import cv2
 
+IMG_SRC = "red_eye_couple.jpg"
+
 def fillHoles(mask):
     """Fill potential holes in a generated mask."""
     maskFloodFill = mask.copy()
@@ -11,18 +13,72 @@ def fillHoles(mask):
     return mask2 | mask
 
 # Read the image
-#img = cv2.imread("test_img/red_eye_photomag_copy.jpg", cv2.IMREAD_COLOR)
+img = cv2.imread("test_img/"+IMG_SRC, cv2.IMREAD_COLOR)
 
 # Create an image copy
-#imgOut = img.copy()
+imgOut = img.copy()
 
 # Load HAAR cascades
 faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 eyesCascade = cv2.CascadeClassifier("haarcascade_eye.xml")
 
 # Load camera feed
-cap = cv2.VideoCapture(0)
+#cap = cv2.VideoCapture(0)
 
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+faces = faceCascade.detectMultiScale(gray, 1.3, 5)
+for (x,y,w,h) in faces:
+    cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 2)
+
+    roiGray = gray[y:y+h, x:x+w]
+    roiColor = img[y:y+h, x:x+w]
+
+    eyes = eyesCascade.detectMultiScale(roiGray)
+    for (ex, ey, ew, eh) in eyes:
+        cv2.rectangle(roiColor, (ex,ey), (ex+ew,ey+eh), (0,255,0), 2)
+        # Extract the individual eye from the larger image
+        eye = img[y:y+h, x:x+w]
+
+        # Split colors into RGB
+        b = eye[:, :, 0]
+        g = eye[:, :, 1]
+        r = eye[:, :, 2]
+
+        # Combine green and blue
+        bg = cv2.add(b, g)
+
+        # Red eye mask
+        mask = (r > 150) & (r > (bg)*1.5)
+
+        # Convert the mask format
+        mask = mask.astype(np.uint8)*255
+
+        # Clean up the mask by filling holes and dilating
+        mask = fillHoles(mask)
+        mask = cv2.dilate(mask, None, anchor=(-1, -1), iterations=3, borderType=1, borderValue=1)
+
+        # Average the blue and green values
+        mean = bg//2
+        mask = mask.astype(np.bool)[:, :, np.newaxis]
+        mean = mean[:, :, np.newaxis]
+
+        # Copy the eye from the original image
+        eyeOut = eye.copy()
+
+        # Copy an averaged eye to the output
+        np.copyto(eyeOut, mean, where=mask)
+
+        # Merge the new eye into the output image
+        img[y:y+h, x:x+w, :] = eyeOut
+
+while True:
+    cv2.imshow("img",img)
+    k = cv2.waitKey(30) & 0xFF
+    if k == 27:
+        break
+cv2.destroyAllWindows()
+
+"""
 while True:
     ret, img = cap.read()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -44,7 +100,7 @@ while True:
 
 cap.release()
 cv2.destroyAllWindows()
-
+"""
 
 """
 # Detect eyes
